fc7fdec0e9abd4682397ad5d85b411e0d000d116
Vanessa <32470478+rdclvanessa@users.noreply.github.com>
2019-11-04 20:28:09 -0600

    classes added

---
+++ InvoiceEntry.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Assignment4
+{
+    class InvoiceEntry
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
