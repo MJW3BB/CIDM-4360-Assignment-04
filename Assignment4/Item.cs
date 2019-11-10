b338cfd4c2c7ba9954d9a78f551fb85b3220cef2
Vanessa Valenzuela <gvvalenzuela1@buffs.wtamu.edu>
2019-11-10 14:40:52 -0600

    a bit more work on item.cs methods

--- Assignment4/Item.cs
+++ Assignment4/Item.cs
@@ -3,64 +3,58 @@ using System;
 namespace Assignment4
 {
     class Item
-    {
-        static void Main(string[] args)
-        {
-           
-            int ID;
-            string description;
-            float availableQty;
-            double unitPrice;        
+    {      
+        public static int ID;
+        public static string description;
+        public static float availableQty;
+        public static double unitPrice;        
         
-            //Updates the current available quantity by add/subtracting
-            //the passed value and returns new quantity
-            static int updateAvlblQty(int qty1, int qty2, int qty3)
-            {
-                qty1 = 30;
-                qty2 = 10;
-                qty3 = 30; 
-
-                return qty1, qty2, qty3; 
-            }
-
-            //Returns the item's unit price
-            static double getPrice(double price1, double price2, double price3)
-            {
-                price1 = 1.99;
-                price2 = 3.99;
-                price3 = 5.99; 
-
-                return price1, price2, price3;
-            }
-
-            //Returns the item's description
-            static string getItemDescription(string description1, string description2, string description3)
-            {
-                description1 = "groceries #1";
-                description2 = "groceries #2";
-                description3 = "groceries #3";
-
-                return description1, description2, description3; 
-            }
-
-            //Returns the item's ID
-            static int getItemID(int ID)
-            {
-                int index1 = 122;
-                int index2 = 124; 
-                int index3 = 126; 
-
-                return index1, index2, index3; 
-                
-            }
-
-            //Displays the item info to console(id, desc, price, avlbl )
-            static string displayItem(string displayItem)
-            {
-                Console.WriteLine($"{getItemID}{getItemDescription}{availableQty}{getPrice}")
+        //Updates the current available quantity by add/subtracting
+        //the passed value and returns new quantity
+        private int updateAvlblQty(int quantity)
+        {
             
-                return displayItem;
-            }
+            
+            return ; 
+        }
+
+        //Returns the item's unit price
+        static (double,double,double) getPrice(double price)
+        {
+            double price1 = 1.99;
+            double price2 = 3.99;
+            double price3 = 5.99; 
+
+            return (price1, price2, price3);
+        }
+
+        //Returns the item's description
+        static string getItemDescription(Item.122, Item.124, Item.126)
+        {
+            description1 = "groceries #1";
+            description2 = "groceries #2";
+            description3 = "groceries #3";
+
+            return (description1, description2, description3); 
+        }
+
+        //Returns the item's ID
+        static int getItemID(int ID)
+        {
+            int index1 = 122;
+            int index2 = 124; 
+            int index3 = 126; 
+
+            return (index1, index2, index3); 
+                
+        }
+
+        //Displays the item info to console(id, desc, price, avlbl )
+        static string displayItem(string displayItem)
+        {
+            Console.WriteLine($"{getItemID}{getItemDescription}{availableQty}{getPrice}");
+        
+            return displayItem;
         }
     }
 }
